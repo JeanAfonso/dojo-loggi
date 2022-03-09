b7c3b2479e0d9714a13f4f95d9d9f97f75739830
JeanAfonso <jean.alves@loggi.com>
2022-03-09 16:15:01 -0300

    Implementing DFS algorithm

--- dojov5.py
+++ dojov5.py
@@ -5,17 +5,20 @@ A unival tree (which stands for “universal value”) is a tree where all nodes
 Given the root to a binary tree, count the number of unival subtrees.
 
 
-    	   (0) -> n1
+    	   (0) -> n1        ------------------ nivel 0
 	      /   \
-  n2 <- (0)   (1) -> n3
+  n2 <- (0)   (1) -> n3     ------------------ nivel 1
               / \
-      n4 <- (1) (1) -> n5
+      n4 <- (1) (1) -> n5   ------------------ nivel 2
             / \   \
-    n6 <- (1) (1) (1) -> n8
+    n6 <- (1) (1) (1) -> n8 ------------------ nivel 3
                ^
                 \
                 n7
 """
+
+# DFS -> depth first search (busca em profundidade): recursão/iterativa (lista)
+# BFS -> breath first search (busca em largura): iterativa (lista)
 
 
 class Node:
@@ -26,14 +29,35 @@ class Node:
         self.parent = None
 
 
-n1 = Node(0)
-n2 = Node(0)
-n3 = Node(1)
-n4 = Node(1)
-n5 = Node(1)
-n6 = Node(1)
-n7 = Node(1)
-n8 = Node(1)
+"""
+Passos:
+- Acessa nó raíz
+- Tenta acessar filho a esquerda, caso não, volta pro anterior
+    - Se tem, entra no filho e ele vira o "raíz"
+    - Volta ao passo 1
+- Tenta acessar filho a direita, caso não, volta pro anterior
+    - Se tem, entra no filho e ele vira o "raíz"
+    - Volta ao passo 1
+"""
+
+
+def dfs(node: Node):
+    print(node.value)  # Acessa nó
+
+    if node.left:  # Tenta acessar filho a esquerda, caso não, volta pro anterior
+        dfs(node.left)  # Se tem, entra no filho e ele vira o "raíz"
+    if node.right:  # Tenta acessar filho a direita, caso não, volta pro anterior
+        dfs(node.right)  # Se tem, entra no filho e ele vira o "raíz"
+
+
+n1 = Node(1)
+n2 = Node(2)
+n3 = Node(3)
+n4 = Node(4)
+n5 = Node(5)
+n6 = Node(6)
+n7 = Node(7)
+n8 = Node(8)
 
 n1.left = n2
 n1.right = n3
@@ -56,3 +80,6 @@ n6.parent = n4
 n7.parent = n4
 
 n8.parent = n5
+
+
+dfs(n1)
